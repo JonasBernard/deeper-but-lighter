260b64c8400699d7e3442ed5aef046bc716c388d
rom <romangraef@gmail.com>
2021-04-24 16:29:15 +0200

    points are every 5pixels you doofus

--- Camera2D.gd
+++ Camera2D.gd
@@ -22,6 +22,7 @@ func _process(delta):
 		pidx += 1
 		if pidx >= points.size():
 			pidx = -1
+		_process(delta - dist.length() / current_speed )
 		return
 	var velocity = dist.normalized() * current_speed * delta
 	position += velocity
