8d91ce158c844d7426c7300c86603767679725cd
Jonas Bernard <public.jbernard@web.de>
2021-04-25 00:56:41 +0200

    pause mode

--- scenes/levels/base_level.gd
+++ scenes/levels/base_level.gd
@@ -6,11 +6,5 @@ onready var pause_menu = $PauseMenu
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_MenuButton_on_click():
-	pause_menu.visible = true
 	pause_menu.pause()
--- scenes/pause.gd
+++ scenes/pause.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
 onready var pause_menu = $"."
+onready var backgroud = $Control/Sprite
 
 func pause():
-	pause_menu.pause_mode = PAUSE_MODE_PROCESS
+	pause_menu.visible = true
+	pause_menu.pause_mode = PAUSE_MODE_PROCESS # to be save
 	get_tree().paused = true
 
+
+func _ready():
+	backgroud.modulate.a = 0.5
+
+
+func _on_Resume_on_click():
+	pause_menu.visible = false
+	get_tree().paused = false
+
+
+func _on_Main_Menu_on_click():
+	get_tree().change_scene("res://scenes/menu.tscn")
