fafd4fc6a6bcd0ac8191f62c315db46dea26864f
Jonas Bernard <public.jbernard@web.de>
2021-04-25 23:10:04 +0200

    New checkmail

--- components/Level.gd
+++ components/Level.gd
@@ -4,9 +4,6 @@ class_name Level
 signal lost_heart()
 signal level_finished()
 
-onready var passwordfield = $password
-var trys = 0
-
 
 func start():
 	print("Level starting.")
@@ -17,32 +14,3 @@ func lose_heart():
 func finish_level():
 	emit_signal("level_finished")
 
-func login():
-	$Incorrect.visible = false
-	get_tree().change_scene("res://scenes/introduction/mail.tscn")
-	finish_level()
-	
-func check_pw():
-	if passwordfield.text == "correct":
-		login()
-	else:
-		if passwordfield.text == "deeper" or trys == 10:
-			$Monolog2.start()
-			$Monolog1.visible = false
-		if trys == 20:
-			$Monolog3.start()
-			$Monolog2.visible = false
-		trys = trys + 1
-		$Incorrect.visible = true
-		passwordfield.text = ""
-
-func _on_Button_on_click():
-	check_pw()
-
-
-func _on_password_text_changed(new_text):
-	$Incorrect.visible = false
-
-
-func _on_password_text_entered(new_text):
-	check_pw()
---
+++ scenes/introduction/check_mail.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+onready var passwordfield = $password
+var trys = 0
+
+func _ready():
+	pass # Replace with function body.
+
+func login():
+	$Incorrect.visible = false
+	get_tree().change_scene("res://scenes/introduction/mail.tscn")
+
+func check_pw():
+	if passwordfield.text == "correct":
+		login()
+	else:
+		if passwordfield.text == "deeper" or trys == 10:
+			$Monolog2.start()
+			$Monolog1.visible = false
+		if trys == 20:
+			$Monolog3.start()
+			$Monolog2.visible = false
+		trys = trys + 1
+		$Incorrect.visible = true
+		passwordfield.text = ""
+
+func _on_password_text_changed(new_text):
+	$Incorrect.visible = false
+
+
+func _on_password_text_entered(new_text):
+	check_pw()
+
+func _on_Log_in_on_click():
+	check_pw()
