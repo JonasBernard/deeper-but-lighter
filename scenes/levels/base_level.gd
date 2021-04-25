26900b16664922203ad658f0f561624114ac601d
Jonas Bernard <public.jbernard@web.de>
2021-04-25 13:06:51 +0200

    Name

--- scenes/levels/base_level.gd
+++ scenes/levels/base_level.gd
@@ -5,6 +5,7 @@ onready var pause_menu = $PauseMenu
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Control/TextReveal.start()
+	$healthbar.set_health(2)
 
 func _on_MenuButton_on_click():
 	pause_menu.pause()
