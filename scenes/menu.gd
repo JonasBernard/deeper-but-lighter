65da988ee9536858ef9975becb739ba24c695aea
Jonas Bernard <public.jbernard@web.de>
2021-04-25 15:23:55 +0200

    Menu animation

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -10,4 +10,9 @@ func _on_Exit_on_click():
 
 
 func _on_Strart_on_click():
+	if not $Outgoing.is_playing():
+		$Outgoing.play("tointro")
+
+
+func _on_Outgoing_animation_finished(anim_name):
 	get_tree().change_scene("res://scenes/introduction.tscn")
