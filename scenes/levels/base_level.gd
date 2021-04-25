e48dd86c4b4b5c406717d10beca03a7e3e526efb
rom <romangraef@gmail.com>
2021-04-25 14:11:45 +0200

    Make pause background darker

--- scenes/levels/base_level.gd
+++ scenes/levels/base_level.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-onready var pause_menu = $PauseMenu
+onready var pause_menu = $Camera2D/PauseMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Control/TextReveal.start()
-	$healthbar.set_health(2)
+	$Camera2D/healthbar.set_health(2)
 
 func _on_MenuButton_on_click():
 	pause_menu.pause()
