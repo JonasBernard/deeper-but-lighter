670c575ee920b730578f2a3eb78c13c08771fe19
rom <romangraef@gmail.com>
2021-04-26 16:15:42 +0200

    settings

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready():
-	pass
+	_difficulty_text()
 
 
 func _on_Exit_on_click():
@@ -15,4 +15,23 @@ func _on_Strart_on_click():
 
 
 func _on_Outgoing_animation_finished(anim_name):
-	get_tree().change_scene("res://scenes/introduction/come home.tscn")
+	if anim_name == "tointro":
+		get_tree().change_scene("res://scenes/introduction/come home.tscn")
+
+
+func _on_Back_on_click():
+	$Outgoing.play_backwards("tosettings")
+
+func _difficulty_text():
+	if Settings.difficulty == Settings.Difficulty.HARD:
+		$Content/CustomLabel.text = "Hard"
+	else:
+		$Content/CustomLabel.text = "Easy"
+
+func _on_HardMode_on_click():
+	Settings.difficulty = 1 - Settings.difficulty
+	_difficulty_text()
+	Settings.save()
+
+func _on_SettingsButton_on_click():
+	$Outgoing.play("tosettings")
